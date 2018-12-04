0eff50a41bf23c35d85cc98f9239726b0c9c16d8
Jason Carr <jcarr250@gmail.com>
2018-12-04 12:03:10 -0500

    Split output intwo two, lock to grid

--- getPolygon.js
+++ getPolygon.js
@@ -21,17 +21,37 @@ var isDone=false;
 var pointsOfPolygon = [];
 var congruentPolygonPoints = [];
 
-$('#done').click(function(){
-  isDone=true;
+$('#done').click(function() {
+  if (!isDone) {
+    isDone=true;
+    calculatePolys();
+  }
+});
+
+function calculatePolys() {
+  pointsOfPolygon = [];
   for(index=0; index<storeCoordinates.length;index++) {
     pointsOfPolygon.push(storeCoordinates[index])
   }
 
   document.getElementById("answer").innerHTML = pointsOfPolygon
   congruentPolygonPoints = splitPolygon(pointsOfPolygon)
-  document.getElementById("output").innerHTML = congruentPolygonPoints
+  document.getElementById("output1").innerHTML = congruentPolygonPoints[0]
+  document.getElementById("output2").innerHTML = congruentPolygonPoints[0]
 
-});
+  if (congruentPolygonPoints != null) {
+    context.strokeStyle = "#DD00DD";
+    drawPolygon(toCoords(congruentPolygonPoints[0]));
+    context.strokeStyle = "#00DDDD";
+    drawPolygon(toCoords(congruentPolygonPoints[1]));
+  }
+}
+
+function toCoords(arr) {
+  return arr.map(function(arr) {
+    return { 'x': arr[0], 'y': arr[1] };
+  })
+}
 
 $("#canvas").mousedown(function(e){handleMouseDown(e);});
 
@@ -44,19 +64,21 @@ function handleMouseDown(e){
 
   mouseX=parseInt(e.clientX-offsetX);
   mouseY=parseInt(e.clientY-offsetY);
+  mouseX = Math.round(mouseX / 50) * 50
+  mouseY = Math.round(mouseY / 50) * 50
   storeCoordinates.push([mouseX, mouseY]);
   coordinates.push({x:mouseX,y:mouseY});
-  drawPolygon();
 
-
+  context.clearRect(0,0,cw,ch);
+  context.strokeStyle = "#00DDDD";
+  drawPolygon(coordinates);
 }
 
-function drawPolygon(){
-  context.clearRect(0,0,cw,ch);
+function drawPolygon(coords){
   context.beginPath();
-  context.moveTo(coordinates[0].x, coordinates[0].y);
-  for(index=1; index<coordinates.length;index++) {
-    context.lineTo(coordinates[index].x, coordinates[index].y);
+  context.moveTo(coords[0].x, coords[0].y);
+  for(index=1; index<coords.length;index++) {
+    context.lineTo(coords[index].x, coords[index].y);
   }
   context.closePath();
   context.stroke();
